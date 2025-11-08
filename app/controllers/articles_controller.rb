4d491f9a9876f0d70c0c32de94e7c44fa6ac9031
MishimaMomoka <m.momoka0032@gmail.com>
2025-11-08 16:26:29 +0900

    変更点しましたよ。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,16 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :correct_user, only: %i[ edit update destroy ]
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
+    @comment = @article.comments.build
   end
 
   # GET /articles/new
@@ -21,11 +24,13 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
+    #@article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
+        #format.html { redirect_to @article, notice: "Article was successfully created." }
+        format.html { redirect_to @article, notice: t('notices.create', model: Article.model_name.human)}
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +43,8 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
+        #format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
+        format.html { redirect_to @article, notice: t('notices.update', model: Article.model_name.human), status: :see_other }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +58,11 @@ class ArticlesController < ApplicationController
     @article.destroy!
 
     respond_to do |format|
-      format.html { redirect_to articles_path, notice: "Article was successfully destroyed.", status: :see_other }
+      format.html do
+        redirect_to articles_path,
+                    notice: I18n.t('notices.destroy', model: Article.model_name.human),
+                    status: :see_other
+      end
       format.json { head :no_content }
     end
   end
@@ -65,6 +75,10 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.expect(article: [ :title, :body, :summary])
+      params.expect(article: [ :title, :body, :summary ])
+    end
+
+    def correct_user
+      redirect_to articles_path, alert: "権限がありません。" unless @article.user == current_user
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,8 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { maximum: 50 }
-    validates :body, presence: true
-end
-
-
-class Article < ApplicationRecord
+    # コメントテーブルとの外部結合
     has_many :comments, dependent: :destroy
-    # 既存バリデーション等があれ維持
+    belongs_to :user
+    # バリデーション
+    validates :title, presence: true, length: {maximum: 50}
+    validates :body, presence: true
 end
