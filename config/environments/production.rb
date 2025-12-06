e35e3749fd26966bece602d1a3b54dcbd135f1dc
MishimaMomoka <m.momoka0032@gmail.com>
2025-12-06 15:34:45 +0900

    管理ページ実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,10 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # 管理画面用 Basic 認証
+  config.x.admin_basic_auth = {
+    user: "admin",
+    password: "password123"
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :articles do
   resources :comments, only: [:create, :destroy]
 end
+
+namespace :admin do
+  root "users#index"
+  resources :users, only: [:index, :show]
+end
+
   # ヘルスチェック用
   get "up" => "rails/health#show", as: :rails_health_check
   # ルートパス(任意)
